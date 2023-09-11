fc37ac4738af2d30dbb718cb3d463877cd068aff
João Vitor Miguez da Cunha <PET0147543@aluno.firjansenaisesi.com.br>
2023-09-11 11:20:00 -0300

    Exercício01

---
+++ app.js
@@ -0,0 +1,15 @@
+//Exercicio 01 
+function exercicio01() {
+    let numero01;
+    let  numero02;
+    let resultado;
+
+    numero01 = parseFloat(window.prompt("Escreva o primeiro número: "));
+    numero02 = parseFloat(window.prompt("Escreva o segundp número: "));
+
+    resultado = numero01 + numero02;
+    alert(resultado);
+
+}
+
+exercicio01();
